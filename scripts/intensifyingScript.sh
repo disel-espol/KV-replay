f8d57bd5fc43d8d0bce53927e204640bdb26144d
jose <josevns95@gmail.com>
2017-03-29 22:15:31 -0500

    adding intense script v3

--- scripts/intensifyingScript.sh
+++ scripts/intensifyingScript.sh
@@ -1,171 +1,102 @@
-declare -A table=()
 
 inputTrace=$1
-let TIF=$2
+TIF=$2
 outputTrace=$3
 typeAlgorithm=${4:-md5}
 separator=,
 
+size=$( wc -l $inputTrace | cut -d" " -f1 )
+#echo "$size"
+perfectSize=$(( size / TIF ))
+#resid=$(( size % TIF))
+#echo "este es perfectSize : $perfectSize"
+
 > $outputTrace
 
-conversion (){
-	foo=$1
-	output=""
-	for (( i=0; i<${#foo}; i++ )); do
-		letter="${foo:$i:1}"
-		LC_CTYPE=C 
-  		a=$(printf '%d' "'$letter")
-  		output="$output$a"
-	done
-
-	echo "$output"	
-}
-
-numberOfSubtraces2=$(( TIF -1))
-let counter=0
-Size=""
-
-
-bool=false
-while IFS=$separator read -ra ARRAY || [[ -n "$ARRAY" ]]; do
-
-	ID="${ARRAY[1]}"
-	Timestamp="${ARRAY[2]}"
-	Command="${ARRAY[0]}"
-	if [ ${#ARRAY[@]} -gt 3 ]
-	then
-		Size="${ARRAY[3]}"
-		#echo -e "\n \n #Size"
-		#echo "$Size"
-		bool=true
-	fi
-
-
-	if [ "$typeAlgorithm" = "ascii" ] ; then
-		let hashed=$(conversion $ID)
-		let temp=$(( $hashed % $TIF ))
-		if [ $temp -lt 0 ];then
-			temp=$(( temp *-1))
-		fi
-	elif [ "$typeAlgorithm" = "sha" ]; then
-		hashed=$(echo -n $ID | sha1sum | cut -d" " -f1) 
-		let temp=$(( 16#$hashed % $TIF ))
-		if [ $temp -lt 0 ];then
-			temp=$(( temp *-1))
-		fi
-	elif [ "$typeAlgorithm" = "onlynumbers" ]; then
-		hashed=$ID
-		let temp=$(( 10#$hashed % $TIF ))
-		if [ $temp -lt 0 ];then
-			temp=$(( temp *-1))
-		fi
-
-	elif [ "$typeAlgorithm" = "tr" ]; then
-		hashed=$(echo "$ID"| tr "[:upper:]" "[:lower:]" |  tr "u-z" "0-5" | tr "k-t" "0-9" | tr "a-j" "0-9" | tr "@\-_%" "6-9") 
-		let temp=$(( 10#$hashed % $TIF ))
-		if [ $temp -lt 0 ];then
-			temp=$(( temp *-1))
-		fi
-	else
-		hashed=$(echo -n $ID | md5sum | cut -d" " -f1) 
-		let temp=$(( 16#$hashed % $TIF ))
-		if [ $temp -lt 0 ];then
-			temp=$(( temp *-1))
-		fi
-
-	fi
-	for (( i=0; i<=$numberOfSubtraces2; i++ ))
-	do
-		#echo "entra aqui 0"
-		if [[ $temp -eq $i ]] 
-		then
-			#echo "entra aqui 1"
-
-			if [ -z "${table[$i] + 1}" ]
-			then
-				arrayOfRecords=()
-				table[$i]=${arrayOfRecords[@]}
-	
-			fi
-			if [ $counter -eq 0 ]
-			then
-				firstTimeStamp=$Timestamp
-				let value=0
-			else
-				#echo "$Timestamp"
-				#echo "$preTimeStamp"
-				#echo "$TIF"
-				value=$(echo "scale=9;($Timestamp - $preTimeStamp )/$TIF" | bc)
-				#echo "value es $value"
-			fi
-			arrayGanador=(${table[$i]})
-			if [ $bool = true ]
-			then
-				arrayGanador+=("$Command,$ID,$value,$Size")
-			else
-				arrayGanador+=("$Command,$ID,$value")
-			fi
-			
-			table[$i]=${arrayGanador[@]}
-			let counter+=1
-			preTimeStamp=$Timestamp
-
-		fi
-
-		#echo "${table[$i]}"
-
-	done
-
-done < "$inputTrace" 
-
-maxLength=0
-for (( i=0; i<=$numberOfSubtraces2; i++ )); do
-	arrayTemp1=(${table[$i]})
-	#echo -e "\n este es el subtrace $i : ${arrayTemp1[@]}"
-	if [[ ${#arrayTemp1[@]} -gt $maxLength ]]; then
-		maxLength=${#arrayTemp1[@]}
-
-	fi
-done
-
-prevTime=$firstTimeStamp
-for (( i=0; i<=$maxLength; i++ ))
+let counter2=0 #contador global
+let oldTime=0
+for (( counter=0 ; counter<$perfectSize ; counter ++ ))
 do
-	for (( j=0; j<=$numberOfSubtraces2; j++ ))
+
+
+	for (( i=0 ; i<$TIF ; i++ ))
 	do
+		let counter2+=1
 
-		arrayTemp2=(${table[$j]})
-		if [ ! -z "${arrayTemp2[$i]}" ]
+		let line=$(( (i * perfectSize) + (counter +1) ))
+		correctValue=$( sed -n "$line p" < $inputTrace)
+		#echo "este es correctValue : $correctValue"
+		key=$( echo "$correctValue" | cut -d"," -f2)
+		#echo " clave es : $key"
+		Command=$( echo "$correctValue" | cut -d"," -f1)
+		#echo "clave es : $Command"
+		if [ $i -gt 9 ]
 		then
-			value2=${arrayTemp2[$i]}
-			Command=$( echo $value2 | cut -d"," -f1 )
-			key=$( echo $value2 | cut -d"," -f2 )
-			timme=$( echo $value2 | cut -d"," -f3 )
-			
-
-			#echo "timme es $timme"
-			newtimme=$(echo "scale=9; $prevTime + $timme" | bc)
-			#echo "newtimme es $newtimme"
-			if [ $bool = true ]
-			then
-				size=$( echo $value2 | cut -d"," -f4 )
-				echo $Command,$key,$newtimme,$size >> $outputTrace
-
-			else
-				echo $Command,$key,$newtimme >> $outputTrace
-			fi			
-			prevTime=$newtimme
-
+			newKey=$i$key
+		else
+			newKey=0$i$key
+		fi
 
 		
+		valueForTime=$( sed -n "$counter2 p" < $inputTrace)
+		#echo "este es valueForTime : $valueForTime "
+		if [ $counter2 -eq 1 ]
+		then
+			newTime=$( echo "$valueForTime" | cut -d"," -f3)
+			correctTime=$newTime
+			#echo " este es correctTime : $correctTime"
 		else
-			continue
-
+			newTime=$( echo "$valueForTime" | cut -d"," -f3)
+			#echo "este es oldTime : $oldTime"
+			#echo "este es newTime : $newTime"
+			correctTime=$(echo "scale=9; ((($newTime - $oldTime ) /$TIF) + $oldCorrectTime)" | bc)
+			#echo " este es correctTime : $correctTime"
 		fi
 
+		sizeRecord=$( echo "$correctValue" | cut -d"," -f4)
+		if [ -z "$sizeRecord" ];then
+			echo $Command,$newKey,$correctTime >> $outputTrace
+		else
+			echo $Command,$newKey,$correctTime,$sizeRecord >> $outputTrace
+		fi
+			
+
+
+
+		oldCorrectTime=$correctTime
+		#echo "este es oldCorrectTime : $oldCorrectTime"
+		oldTime=$newTime
+		#echo -e "\n"
+		
 	done
 
+done 
+
+#echo -e "\n \n ZONA DE RESIDUOS"
+#echo "este es counter2 : $counter2"
+while [ $counter2 -lt $size ]
+do
+	let counter2+=1
+	#echo "este es counter2 : $counter2"
+	resid=$(( counter2 % TIF ))
+	valueForTime=$( sed -n "$counter2 p" < $inputTrace)
+	#echo "este es valueForTime : $valueForTime"
+	key=$( echo "$valueForTime" | cut -d"," -f2)
+	if [ $resid -gt 9 ]
+	then
+		newKey=$resid$key
+	else
+		newKey=0$resid$key
+	fi
+	Command=$( echo "$valueForTime" | cut -d"," -f1)
+	newTime=$( echo "$valueForTime" | cut -d"," -f3)
+	correctTime=$(echo "scale=9; ((($newTime - $oldTime ) /$TIF) + $oldCorrectTime)" | bc)
+	sizeRecord=$( echo "$valueForTime" | cut -d"," -f4)
+	if [ -z "$sizeRecord" ];then
+		echo $Command,$newKey,$correctTime >> $outputTrace
+	else
+		echo $Command,$newKey,$correctTime,$sizeRecord >> $outputTrace
+	fi
+	oldCorrectTime=$correctTime
+	oldTime=$newTime
 done
-
-
-
