a511a38cb9a0f0cec54bcf170e30a8b86722c5f8
jose <josevns95@gmail.com>
2017-03-31 23:55:18 -0500

    modified intense script to v5

--- scripts/intensifyingScript.sh
+++ scripts/intensifyingScript.sh
@@ -1,83 +1,63 @@
-
 inputTrace=$1
 TIF=$2
 outputTrace=$3
 
 size=$( wc -l $inputTrace | cut -d" " -f1 )
 perfectSize=$(( size / TIF ))
-> $outputTrace
-
-let counter2=0 
-let oldTime=0
-for (( counter=0 ; counter<$perfectSize ; counter ++ ))
+bool=false
+for (( i=0 ; i<$TIF ; i++ ))
 do
+	initial=$(( (i * $perfectSize) + 1 ))
+	final=$(( (i + 1) * perfectSize ))
+	sed -n "$initial, $final p" < $inputTrace > $inputTrace-temporalSubtrace-$i
+	correctValue=$(head -n 1 $inputTrace-temporalSubtrace-$i)
+	firstCommand=$( echo "$correctValue" | cut -d"," -f1 )
+	firstKey=$( echo "$correctValue" | cut -d"," -f2 )
+	firstTimeStamp=$( echo "$correctValue" | cut -d"," -f3 )
+	firstSize=$( echo "$correctValue" | cut -d"," -f4 )
+	if [ -z "$firstSize" ];then
+		echo $firstCommand,$firstKey,0 > $inputTrace-temporalSubtrace-awk-$i 
+	else
+		bool=true
+		echo $firstCommand,$firstKey,0,$firstSize > $inputTrace-temporalSubtrace-awk-$i
+	fi
+	awk -F , -v OFS=, 'BEGIN {CONVFMT = "%.9f"} ;  $3 -= "'"$firstTimeStamp"'" ' < $inputTrace-temporalSubtrace-$i >> $inputTrace-temporalSubtrace-awk-$i
 
+	rm $inputTrace-temporalSubtrace-$i
 
-	for (( i=0 ; i<$TIF ; i++ ))
-	do
-		let counter2+=1
-
-		let line=$(( (i * perfectSize) + (counter +1) ))
-		correctValue=$( sed -n "$line p" < $inputTrace)
-		key=$( echo "$correctValue" | cut -d"," -f2)
-		Command=$( echo "$correctValue" | cut -d"," -f1)
-		if [ $i -gt 9 ]
-		then
-			newKey=$i$key
-		else
-			newKey=0$i$key
-		fi
-
-		
-		valueForTime=$( sed -n "$counter2 p" < $inputTrace)
-		if [ $counter2 -eq 1 ]
-		then
-			newTime=$( echo "$valueForTime" | cut -d"," -f3)
-			correctTime=$newTime
-		else
-			newTime=$( echo "$valueForTime" | cut -d"," -f3)
-			correctTime=$(echo "scale=9; ((($newTime - $oldTime ) /$TIF) + $oldCorrectTime)" | bc)
-		fi
-
-		sizeRecord=$( echo "$correctValue" | cut -d"," -f4)
-		if [ -z "$sizeRecord" ];then
-			echo $Command,$newKey,$correctTime >> $outputTrace
-		else
-			echo $Command,$newKey,$correctTime,$sizeRecord >> $outputTrace
-		fi
-			
-
-
-
-		oldCorrectTime=$correctTime
-		oldTime=$newTime
-		
-	done
-
-done 
-
-
-while [ $counter2 -lt $size ]
-do
-	let counter2+=1
-	resid=$(( counter2 % TIF ))
-	valueForTime=$( sed -n "$counter2 p" < $inputTrace)
-	key=$( echo "$valueForTime" | cut -d"," -f2)
-	if [ $resid -gt 9 ]
+	iput=0$i
+	if [ $i -gt 9 ]
 	then
-		newKey=$resid$key
-	else
-		newKey=0$resid$key
+		iput=$i
 	fi
-	Command=$( echo "$valueForTime" | cut -d"," -f1)
-	newTime=$( echo "$valueForTime" | cut -d"," -f3)
-	correctTime=$(echo "scale=9; ((($newTime - $oldTime ) /$TIF) + $oldCorrectTime)" | bc)
-	sizeRecord=$( echo "$valueForTime" | cut -d"," -f4)
-	if [ -z "$sizeRecord" ];then
-		echo $Command,$newKey,$correctTime >> $outputTrace
+
+
+	if [ $bool = false ]
+	then
+		awk -v OFS="," -F"," '{print $1,"'"$iput"'"$2,$3}' $inputTrace-temporalSubtrace-awk-$i > $inputTrace-temporalSubtrace-awk-$i-two
 	else
-		echo $Command,$newKey,$correctTime,$sizeRecord >> $outputTrace
+		awk -v OFS="," -F"," '{print $1,"'"$iput"'"$2,$3,$4}' $inputTrace-temporalSubtrace-awk-$i > $inputTrace-temporalSubtrace-awk-$i-two
 	fi
-	oldCorrectTime=$correctTime
-	oldTime=$newTime
+
+	rm $inputTrace-temporalSubtrace-awk-$i
+
 done
+
+
+for ((i=1 ; i<$TIF ; i++ ))
+do
+	j=$(( i - 1 ))
+	sort -t, -k3 -n -m $inputTrace-temporalSubtrace-awk-$j-two $inputTrace-temporalSubtrace-awk-$i-two > $inputTrace-temporalSubtrace-awk-temp-two 
+	mv $inputTrace-temporalSubtrace-awk-temp-two $inputTrace-temporalSubtrace-awk-$i-two
+done
+
+for ((i=1 ; i<$TIF ; i++ ))
+do
+	TIFminus1=$(( TIF -1 ))
+	if [ $TIFminus1 -eq $i ]; then
+		mv $inputTrace-temporalSubtrace-awk-$i-two $outputTrace
+	else
+		rm $inputTrace-temporalSubtrace-awk-$i-two
+	fi
+done
+
