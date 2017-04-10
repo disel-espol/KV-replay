04fc05be113b301a3938bf0c085705ea7565b4a6
jose <josevns95@gmail.com>
2017-04-09 23:24:29 -0500

    add intensifying script v7

--- scripts/intensifyingScript.sh
+++ scripts/intensifyingScript.sh
@@ -1,14 +1,45 @@
 inputTrace=$1
 TIF=$2
 outputTrace=$3
+flag=${4:-true}
 
 size=$( wc -l $inputTrace | cut -d" " -f1 )
 perfectSize=$(( size / TIF ))
 bool=false
+
+if [ "$flag" = true ]
+then
+	firstTimeStampRegister=$(head -n 1 $inputTrace)
+	lastTimeStampRegister=$(tail -1 $inputTrace)
+	firstTimeStamp=$( echo "$firstTimeStampRegister" | cut -d"," -f3 )
+	lastTimeStamp=$( echo "$lastTimeStampRegister" | cut -d"," -f3 )
+
+	perfectSize=$( echo "scale=9; ($lastTimeStamp - $firstTimeStamp) / $TIF " | bc )
+	declare -A table=()
+
+
+	for ((i=0 ; i <$TIF ; i++ ))
+	do
+		particularRegister=$( awk -F, -v "iawk=$i" -v "perfectSizeAwk=$perfectSize" -v "firstTimeStampAwk=$firstTimeStamp" '$3 >= ( firstTimeStampAwk + ( iawk * perfectSizeAwk)) { print NR  ; exit }' $inputTrace )
+		table[$i]=$particularRegister
+	done
+	table[$TIF]=$((size +1 ))
+fi
+
+
+
 for (( i=0 ; i<$TIF ; i++ ))
 do
-	initial=$(( (i * $perfectSize) + 1 ))
-	final=$(( (i + 1) * perfectSize ))
+	if [ "$flag" = true ]
+	then
+		initial=${table[$i]}
+		iplus1=$(( i +1))
+		final=$(( ${table[$iplus1]} - 1))
+	else
+		initial=$(( (i * $perfectSize) + 1 ))
+		final=$(( (i + 1) * perfectSize ))
+	fi
+
 	sed -n "$initial, $final p" < $inputTrace > $inputTrace-temporalSubtrace-$i
 	correctValue=$(head -n 1 $inputTrace-temporalSubtrace-$i)
 	firstCommand=$( echo "$correctValue" | cut -d"," -f1 )
@@ -51,7 +82,7 @@ do
 	mv $inputTrace-temporalSubtrace-awk-temp-two $inputTrace-temporalSubtrace-awk-$i-two
 done
 
-for ((i=1 ; i<$TIF ; i++ ))
+for ((i=0 ; i<$TIF ; i++ ))
 do
 	TIFminus1=$(( TIF -1 ))
 	if [ $TIFminus1 -eq $i ]; then
